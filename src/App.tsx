add4fb37a35d2a0950b82ab2ab9acee477bf13ab
brianlynn2 <brian.lynn@global-emarkets.com>
2024-05-19 16:33:01 -0400

    first semi-working version of Brian site under vite / amplify sources - merged code - switched to jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import './App.css';
+import {  BrowserRouter, Routes, Route, useNavigate, useSearchParams, useHistory } from 'react-router-dom'
+import React, { useState, useEffect, useRef } from "react";
+import { createBrowserHistory } from "history";
+import BrianSite from './components/BrianSite.jsx';
+import doScroll from './components/Scroller.jsx'
 
 const client = generateClient<Schema>();
+const history = createBrowserHistory();;
+//const myNav = useNavigate();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -23,32 +30,80 @@ function App() {
     client.models.Todo.delete({ id })
   }
 
+  function renderAuthToDoList() {
+
   return (
 
-    <Authenticator>
-      {({ signOut, user }) => (
-            <main>
-              <h1>{user?.signInDetails?.loginId}'s todos</h1>
-              <button onClick={createTodo}>+ new</button>
-              <ul>
-                {todos.map((todo) => (
-                  <li
-                  onClick={() => deleteTodo(todo.id)}
-                  key={todo.id}>{todo.content}</li>
-                ))}
-              </ul>
-              <div>
-                🥳 App successfully hosted. Try creating a new todo.
-                <br />
-                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                  Review next step of this tutorial.
-                </a>
-              </div>
-              <button onClick={signOut}>Sign out</button>
-            </main>
-      )}
-    </Authenticator>
-  );
+        <Authenticator>
+          {({ signOut, user }) => (
+                <main>
+                  <h1>{user?.signInDetails?.loginId}'s todos</h1>
+                  <button onClick={createTodo}>+ new</button>
+                  <ul>
+                    {todos.map((todo) => (
+                      <li
+                      onClick={() => deleteTodo(todo.id)}
+                      key={todo.id}>{todo.content}</li>
+                    ))}
+                  </ul>
+                  <div>
+                    🥳 App successfully hosted. Try creating a new todo.
+                    <br />
+                    <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+                      Review next step of this tutorial.
+                    </a>
+                  </div>
+                  <button onClick={signOut}>Sign out</button>
+                </main>
+          )}
+        </Authenticator>
+      );
+    }
+
+
+  function renderBrianSite() {
+
+      var ret =  routeSite();
+
+        useEffect(() => {
+          // Update the document title using the browser API
+          doScroll();
+        });
+
+      return <main>{ret}</main>
+  }
+
+
+function routeSite() {
+  const nav = useNavigate();
+  let [searchParams, setSearchParams] = useSearchParams();
+
+   return (
+            <Routes>
+                {createRoute("/", "", nav, searchParams)}
+                {createRoute("/bio", "Bio", nav, searchParams)}
+                {createRoute("/bio?topic=*", "Bio", nav, searchParams)}
+                {createRoute("/port", "Portfolio", nav, searchParams)}
+                {createRoute("/news", "News", nav, searchParams)}
+                {createRoute("/book", "Book", nav, searchParams)}
+            </Routes>
+        );
 }
+
+
+function createRoute (path, sel, nav, searchParams) {
+
+   var elem = <BrianSite sel={sel} hist={history} nav={nav} searchParams={searchParams} />;
+    return (
+        <Route path={path} element={elem} />
+        );
+}
+
+
+  return (renderBrianSite());
+    //return ( renderAuthToDoList());
+  // return <main><BrianSite /></main>;
+  // return <main><h1>test</h1></main>;
+ }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { BrowserRouter } from 'react-router-dom';
+import { doScroll} from './components/Scroller.jsx';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+setTimeout(doScroll, 300);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
